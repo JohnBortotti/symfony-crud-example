bd15caa6ce92f37b08fad270a93fe43dc56b8a1b
JoaoBortotti <joao.bortotti@uotz.com.br>
2022-03-09 13:43:55 -0300

    Cria entidade Student
    Cria entidade Student

---
+++ src/Entity/Student.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Entity;
+
+use App\Domain\Student\Exceptions\StudentDomainException;
+use App\Domain\Student\Interfaces\StudentResourceableInterface;
+use App\Repository\StudentRepository;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StudentRepository::class)]
+#[ORM\Table(name: "students")]
+class Student implements StudentResourceableInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    private $email;
+
+    #[ORM\Column(type: 'date')]
+    private $birthdate;
+
+    #[ORM\Column(type: "datetime", nullable: true)]
+    private $deleted_at;
+
+    #[ORM\OneToMany(mappedBy: 'student_id', targetEntity: Registration::class)]
+    private $registrations;
+
+    public function __construct()
+    {
+        $this->registrations = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getBirthdate(): ?\DateTimeInterface
+    {
+        return $this->birthdate;
+    }
+
+    public function setBirthdate(\DateTimeInterface $birthdate): ?self
+    {
+        $now = new DateTime();
+        $interval = $now->diff($birthdate);
+
+        if ($interval->y < 16) return throw new StudentDomainException('student must be older than 15');
+
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    public function getDeletedAt(): ?\DateTimeInterface
+    {
+        return $this->deleted_at;
+    }
+
+    public function setDeletedAt(\DateTimeInterface $deleted_at): self
+    {
+        $this->deleted_at = $deleted_at;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Registration[]
+     */
+    public function getRegistrations(): Collection
+    {
+        return $this->registrations;
+    }
+
+    public function addRegistration(Registration $registration): self
+    {
+        if (!$this->registrations->contains($registration)) {
+            $this->registrations[] = $registration;
+            $registration->setStudentId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRegistration(Registration $registration): self
+    {
+        if ($this->registrations->removeElement($registration)) {
+            // set the owning side to null (unless already changed)
+            if ($registration->getStudentId() === $this) {
+                $registration->setStudentId(null);
+            }
+        }
+
+        return $this;
+    }
+}
